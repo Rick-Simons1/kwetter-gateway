61ce688f585cb5c524035bbcc2417ef03af295a1
Rick-Simons1 <ricksimons12345@yahoo.nl>
2022-06-13 14:12:57 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: 'http://localhost:4200',
+    origin: [
+      'http://localhost:4200',
+      'https://ashy-pebble-0272e8b03.1.azurestaticapps.net',
+    ],
   });
   await app.listen(4000);
 }
